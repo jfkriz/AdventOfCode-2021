aaa38904c17ca32ee394529e2c0d85cb35c9001f
jfkriz <jim@kriz.net>
2021-12-02 13:14:00 +0000

    Day 2, part 1 and 2

---
+++ Day02/1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+try {
+    let fileName = path.resolve(__dirname, 'input.txt');
+    if(process.argv.length > 2) {
+        fileName = process.argv[2];
+    }
+    const data = fs.readFileSync(fileName, 'utf8');
+    processInput(data);
+} catch(e) {
+    console.error(e);
+}
+
+function processInput(data) {
+    let horizontal = 0;
+    let vertical = 0;
+    const input = data.split('\n').map(line => {
+        const pair = line.split(" ");
+        return {
+            "command": pair[0].toLowerCase(),
+            "units": parseInt(pair[1])
+        };
+    })
+
+    input.forEach(i => {
+        switch(i.command) {
+            case "forward":
+                horizontal += i.units;
+                break;
+            case "up":
+                vertical -= i.units;
+                break;
+            case "down":
+                vertical += i.units;
+                break;
+            default:
+                console.log(`Unknown command "${i.command}"" with units ${i.units}`);
+                break;
+        }
+    });
+
+    console.log(`Final Position: Horizontal ${horizontal}, Depth ${vertical}, Answer is ${horizontal * vertical}`);
+}
